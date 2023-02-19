b779a6e5b58d5c85991d755758e8c5ca2e38496f
Connor Darling <connordarling@me.com>
2023-02-19 15:11:39 -0800

    560

---
+++ two-pointer-and-sliding-window/medium/560.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int res = 0;
+        std::unordered_map<int, int> map;
+        int sum = 0; 
+        ++map[0];
+        for (int i = 0; i < nums.size(); ++i) {
+            sum  += nums[i];
+            int diff = sum - k;
+            
+            if (map.count(diff) > 0) {
+                res += map[sum - k];
+            }
+            ++map[sum];
+        }
+
+        return res;
+    }
+};
