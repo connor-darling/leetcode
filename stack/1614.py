3d4ef7255aada22d001b48f5f07fb7e34d9e0f94
Connor Darling <connordarling@me.com>
2025-10-23 22:53:07 -0700

    1614

--- stack/1614.py
+++ stack/1614.py
@@ -4,13 +4,12 @@ class Solution:
     def maxDepth(self, s: str) -> int:
         count = 0
         best = 0
+
         for i in range(len(s)):
             if s[i] == "(":
                 count += 1
             elif s[i] == ")":
                 count -= 1
-            else:
-                continue
 
             best = max(count, best)
 
